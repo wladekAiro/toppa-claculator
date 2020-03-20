40cc9b1b4608731eb5f4201864e1d6a5f3d8b0c2
Wladek Airo <wladek.airo@gmail.com>
2020-03-20 13:23:57 +0300

    fix lint errors

--- src/sum.js
+++ src/sum.js
@@ -1,7 +1,5 @@
 const sum = (a, b, ...moreNumbers) => {
-    const numbers = [a, b, ...moreNumbers]
-        .map((n) => parseInt(n, 10))
-        .filter((n) => !Number.isNaN(n));
+    const numbers = [a, b, ...moreNumbers].map((n) => parseInt(n, 10)).filter((n) => !Number.isNaN(n));
 
     return numbers.reduce((total, n) => total + n, 0);
 };
