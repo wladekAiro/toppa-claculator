324a02abeca019c2dff4e587e92dcbc85f7ae238
Wladek Airo <wladek.airo@gmail.com>
2020-03-20 12:44:43 +0300

    fix lint errors

--- src/sum.js
+++ src/sum.js
@@ -2,8 +2,8 @@ const sum = (a, b, ...moreNumbers) => [a, b, ...moreNumbers].reduce((total, n) =
 
 export default sum;
 
-/* 
+/*
   1. Add two numbers
   2. Add more than two numbers
   3. handle edge cases
- */
+*/
